c6f6df042a20481e792befa54698df70ccc7c6b0
Gabriel Rubens <grsabreu@gmail.com>
2015-10-10 16:52:43 -0300

    Publish for friends list

---
+++ app/server/publications/nearby_users.js
@@ -0,0 +1,23 @@
+Meteor.publish('nearbyUsers', function(location){
+  return Meteor.users.find({
+    _id: {
+      $ne: this.userId
+    },
+
+    location: {
+      $nearSphere: {
+        $geometry: {
+          type: 'Point',
+          coordinates: [ location.lng, location.lat ]
+        },
+
+        $maxDistance: 100
+      }
+    }
+  }, {
+    fields: {
+      profile: 1,
+      'facebook.name': 1
+    }
+  });
+});
