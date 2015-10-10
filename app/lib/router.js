4840622864c588662931b39dc37d168ed5dbe494
Guilherme Decampo <guilherme.decampo@gmail.com>
2015-10-10 09:36:40 -0300

    Remove initial meteor files add some structure

---
+++ app/lib/router.js
@@ -0,0 +1,25 @@
+/**
+ * Routes
+ */
+
+FlowRouter.route('sign', {
+  name: 'sign',
+  action: function() {
+    BlazeLayout.render('Sign');
+  }
+});
+
+FlowRouter.route('friends_list', {
+  name: 'friendsList',
+  action: function() {
+    BlazeLayout.render('FriendsList');
+  }
+});
+
+FlowRouter.route('game/:_id', {
+  name: 'game',
+  action: function() {
+    BlazeLayout.render('Game');
+  }
+});
+
